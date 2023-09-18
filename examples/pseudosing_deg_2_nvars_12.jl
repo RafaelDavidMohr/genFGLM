be9f8542fff1871ae732f8b77bd94829fdc92fa2
Rafael Mohr <Rafael.Mohr@lip6.fr>
2023-09-18 17:17:00 +0200

    replace using Oscar -> using genFGLM

--- examples/pseudosing_deg_2_nvars_12.jl
+++ examples/pseudosing_deg_2_nvars_12.jl
@@ -1,4 +1,4 @@
-using Oscar
+using genFGLM
 P, (x1, x2, x3, x4, x5, x6, y1, y2, y3, y4, y5, y6)  = PolynomialRing(GF(1840109), ["x1", "x2", "x3", "x4", "x5", "x6", "y1", "y2", "y3", "y4", "y5", "y6"]);
 id = ideal(P,F);
 gb = gen_fglm(id, target_order=:degrevlex);
