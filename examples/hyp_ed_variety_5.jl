f73e1560ffed7ff25f1e773052012699a36e8e9b
Rafael Mohr <Rafael.Mohr@lip6.fr>
2023-09-18 17:13:15 +0200

    add some example files

---
+++ examples/hyp_ed_variety_5.jl
@@ -0,0 +1,8 @@
+using genFGLM
+
+P, (x,y,z) = PolynomialRing(GF(1618453), ["x", "y", "z"])
+f = rand_poly_dense(P, 5)
+id = ed_variety([f])
+gb = gen_fglm(id, target_order = :degrevlex)
+display(gb)
+exit()
