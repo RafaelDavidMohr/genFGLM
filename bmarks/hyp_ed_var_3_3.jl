365931ba8772c36a70534ae44224f41422ba2698
Rafael Mohr <Rafael.Mohr@lip6.fr>
2024-05-03 12:25:49 +0200

    update bmark files

--- bmarks/hyp_ed_var_3_3.jl
+++ bmarks/hyp_ed_var_3_3.jl
@@ -8,3 +8,23 @@ vrs = gens(base_ring(id))
 comp_id = ideal(base_ring(id), [vrs[1]*vrs[2], vrs[1]*vrs[3], vrs[2]*vrs[3]])
 gen_fglm(comp_id, just_hypersurface=true)
 @time gen_fglm(id, just_hypersurface=true);
+
+println("-----------")
+println("comparing to elim method")
+gb = groebner_basis_f4(id, complete_reduction = true)
+lts = (leading_monomial).(gens(gb))
+mis = genFGLM.maximal_ind_set(lts)
+isok = false
+R = base_ring(id)
+k = 1
+while k <= length(gens(R))
+    if mis == gens(R)[k:end]
+        global isok = true
+        break
+    end
+    global k += 1
+end
+println("MIS is last variables: $(isok)")
+tim2 = @elapsed groebner_basis_f4(id, eliminate = k - 2, info_level = 2)
+println("time elim $(tim2)")
+exit()
